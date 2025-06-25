0637f5407df504070e324e6a67ab404d6ba0e56c
tscltd <163610737+tscltd@users.noreply.github.com>
2025-06-25 11:44:18 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
-import CategoryMenu from '@/components/CategoryMenu';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
